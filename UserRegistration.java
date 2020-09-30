088ef345f9f47c1c52dce4a7e6baddcd6bd5ae6f
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-30 21:33:52 +0530

    Added: Welcome message added

---
+++ UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabs.userregistration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		// welcome statement
+		System.out.println("Welcome to User Registration Portal!");
+	}
+
+}
